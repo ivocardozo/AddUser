e38a149cea61f60a99eb6abc940af28c603474c9
Ivo Cardozo <ivo.cardozo@ip-192-168-17-98.us-west-2.compute.internal>
2022-03-24 11:52:54 -0400

    adding state and binding inputs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,15 +1,41 @@
+import { useState } from 'react';
+
 import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 
 const AddUser = props => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const addUserHandler = event => {
+    console.log(enteredUsername, enteredAge);
+    event.preventDefault();
+    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if(+enteredAge < 1) {
+      return;
+    }
+    setEnteredUsername('');
+    setEnteredAge('');
+  };
+
+  const usernameChangeHandler = event => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const userAgeChangeHandler = event => {
+    setEnteredAge(event.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form>
         <label htmlFor='username'>Username</label>
-        <input id='username'/>
+        <input id='username' value={enteredUsername} onChange={usernameChangeHandler}/>
         <label htmlFor='age'>Age</label>
-        <input id='age'/>
-        <button>Add User</button>
+        <input id='age' value={enteredAge} onChange={userAgeChangeHandler}/>
+        <button onClick={addUserHandler}>Add User</button>
       </form>
     </Card>
   );
